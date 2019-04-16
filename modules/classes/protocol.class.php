356d6a0d186b5150e17524b5230aae76d1cd5950
Eric Quinton <eric.quinton@irstea.fr>
2019-04-16 16:12:21 +0200

    :feat protocol: début de création de la classe

---
+++ modules/classes/protocol.class.php
@@ -0,0 +1,49 @@
+<?
+/**
+ * ORM of the table protocol
+ */
+class protocol extends ObjetBDD
+{
+
+    /**
+     *
+     * @param PDO $bdd
+     * @param array $param
+     */
+    function __construct($bdd, $param = array())
+    {
+        $this->table = "protocol";
+        $this->colonnes = array(
+            "protocol_id" => array(
+                "type" => 1,
+                "key" => 1,
+                "requis" => 1,
+                "defaultValue" => 0
+            ),
+            "protocol_name" => array(
+                "type" => 0,
+                "requis" => 1
+            ),
+            "protocol_url" => array("type"=>0),
+            "protocol_description"=>array("type"=>0),
+            "measure_default"=>array('type'=>0, "requis"=>1),
+            "measure_default_only"=>array("type"=>1, "requis"=>1),
+            "analysis_template_id"=>array("type"=>1)
+        );
+        parent::__construct($bdd, $param);
+    }
+
+    /**
+     * Return the list of the protocols
+     *
+     * @return array
+     */
+    function getListProtocol() {
+        $sql = "select protocol_id, protocol_name, protocol_url, protocol_description,
+        measure_default, measure_default_only, 
+        analysis_tempate_id, analysis_template_name
+        from protocol
+        order by protocol_name";
+        return $this->getListeParam($sql);
+    }
+}
